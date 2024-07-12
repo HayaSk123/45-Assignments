9de3a7c41a2352dc6117c450cb1af8211e030ce4
HayaSk123 <154345035+HayaSk123@users.noreply.github.com>
2024-07-12 13:02:50 +0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+"use strict";
+//creating a variable and storing my name in it
+let myName = "Haya";
+//creating a another variable and storing a message in it
+let message = `Hello ${myName}, would you like to learn some Python today?`;
+//printing the message
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,9 @@
+//creating a variable and storing my name in it
+
+let myName : string = "Haya"
+
+//creating a another variable and storing a message in it
+let message : string = `Hello ${myName}, would you like to learn some Python today?`
+
+//printing the message
+console.log(message)
